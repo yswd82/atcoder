b5b39da4e4deea0de98e68dc74319893b83e49c5
yswd82 <y.sawada.82@gmail.com>
2024-06-02 09:53:39 +0900

    20240602v2

---
+++ abc234/a.py
@@ -0,0 +1,23 @@
+# N = int(input())
+# N, M = map(int, input().split())
+
+# A = list(map(int, input().split()))
+
+# B = []
+# for i in range(N):
+#     B.append(int(input()))
+
+# S = []
+# for i in range(N):
+#     S.append(input())
+
+# ans = False
+# print('Yes') if ans else print('No')
+t = int(input())
+
+def f(x):
+    return x ** 2 + 2 * x + 3
+
+ans =f(f( f(t) + t) + f(f(t)))
+
+print(ans)
